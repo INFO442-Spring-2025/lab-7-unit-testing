b0e93138f41395884e1be6e40a95769964d02df0
harrisonjohn28 <harrisonjohn28@gmail.com>
2025-05-13 16:12:59 -0700

    Continuing development of lab assignment

--- lab-7/src/App.js
+++ lab-7/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Card from './components/Card';
+import Form from './components/Form';
 import './components/css/Card.css'
+import './components/css/Form.css'
 
 import PROFS from '../src/data/profs.json'
 
@@ -12,23 +14,17 @@ const profsArray = PROFS.map((profElem) => {
   )
 })
 
-console.log(PROFS)
-
-const userObj = {
-  name: "John Harrison",
-  favColor: "kelly green",
-  img: "./img/john.jpg",
-  url: "https://canvas.uw.edu"
-}
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        This is lab 7!
+        John's previous TA gigs!
         <div class="profs-box">
           {profsArray}
         </div>
+
+        What courses have you taken?
+        <Form data={PROFS}/>
       </header>
     </div>
   );
