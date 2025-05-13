ea6bc79d7abb4c462f5629cc1113ff81e84a0da9
harrisonjohn28 <harrisonjohn28@gmail.com>
2025-05-13 14:09:05 -0700

    Begin structuring cards and importing data

--- lab-7/src/App.js
+++ lab-7/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
+import './components/css/Card.css'
+
+import PROFS from '../src/data/profs.json'
+
+const profsArray = PROFS.map((profElem) => {
+  return (
+    <Card
+    key={profElem.name}
+    data={profElem}/>
+  )
+})
+
+console.log(PROFS)
+
+const userObj = {
+  name: "John Harrison",
+  favColor: "kelly green",
+  img: "./img/john.jpg",
+  url: "https://canvas.uw.edu"
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This is lab 7!
+        <div class="profs-box">
+          {profsArray}
+        </div>
       </header>
     </div>
   );
