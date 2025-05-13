8b26eaa29d4398d6b0e8e46e4e81c4d283979b54
harrisonjohn28 <harrisonjohn28@gmail.com>
2025-05-13 16:11:25 -0700

    Change Card styling and props to fit assignment

--- lab-7/src/components/Card.js
+++ lab-7/src/components/Card.js
@@ -3,17 +3,19 @@ export default function Card(props) {
     const data = props.data;
 
     const name = data.name;
-    const favColor = data.favColor;
+    const role = data.role;
+    const course = data.course;
     const img = data.img;
-    const url = data.url;
+    const netId = data.netId;
 
     return (
         <div class="card">
-            <img src={img} class="card-img-top" alt="alt text"/>
+            <img src={img} class="card-img-top" alt={"Headshot of" + name}/>
             <div class="card-body">
                 <h5 class="card-title">{name}</h5>
-                <p class="card-text">{favColor}</p>
-                <a href={url} class="btn btn-primary">Go somewhere</a>
+                <p class="card-text">{role}</p>
+                <p class="card-text">{course}</p>
+                <a href={"https://ischool.uw.edu/people/faculty/profile/"+netId} class="btn btn-primary">View on iSchool</a>
             </div>
         </div>
 
